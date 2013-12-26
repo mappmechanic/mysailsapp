b83a99c1336e9abd81c6642c7179300102eab4a4
mappmechanic <rahat.khanna@genpact.com>
2013-12-26 17:21:03 +0530

    iOS App Enterprise Distribution Experiment

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,13 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
+  },
+
+  '/download': {
+    view: 'download.html'
   }
+
+
 
   /*
   // But what if you want your home page to display
